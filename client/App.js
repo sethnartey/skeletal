3b4785dc3f51b7d66beaf8b3f41da35c2a35f465
Tema Methodist Diocese <sethbilly.nartey@gmail.com>
2022-07-18 21:10:07 +0000

    Add hot module to enable live reloading of frontend assets

--- client/App.js
+++ client/App.js
@@ -3,6 +3,8 @@ import MainRouter from './MainRouter'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from "@material-ui/styles"
 import theme from './theme'
+import { hot } from 'react-hot-loader'
+
 
 const App = () =>
 {
@@ -14,3 +16,5 @@ const App = () =>
         </BrowserRouter>
     )
 }
+
+export default hot(module)(App)
