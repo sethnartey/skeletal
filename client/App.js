d329b005b32dd7c97259fbe12dfdc29a14e0ca7c
Tema Methodist Diocese <sethbilly.nartey@gmail.com>
2022-07-18 21:04:03 +0000

    Add react root component for frontend

---
+++ client/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import MainRouter from './MainRouter'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from "@material-ui/styles"
+import theme from './theme'
+
+const App = () =>
+{
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <MainRouter />
+            </ThemeProvider>
+        </BrowserRouter>
+    )
+}
